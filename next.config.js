9413e353debbc7cb633b56b38ce5db92e633ba46
unknown <fatih969692@gmail.com>
2021-08-14 10:28:27 +0300

    day four build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com']
+    },
+    env:{
+        mapbox_key:'pk.eyJ1IjoiZmF0aWgyNTUiLCJhIjoiY2tzYjZwaGdlMDE3ZjJxbzRqbHBpZnFxbiJ9.ZOH7ia_Kv2PAN4FdVYAr1A'
     }
 }
